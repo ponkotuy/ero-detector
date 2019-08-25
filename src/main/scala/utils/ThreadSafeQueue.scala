815c76c068ce9f42d682ba17c1768f6c825f7764
ponkotuy <web@ponkotuy.com>
2019-08-26 01:59:40 +0900

    Fix import bug

--- src/main/scala/utils/ThreadSafeQueue.scala
+++ src/main/scala/utils/ThreadSafeQueue.scala
@@ -2,6 +2,8 @@ package utils
 
 import java.util.concurrent.LinkedBlockingQueue
 
+import scala.collection.JavaConverters._
+
 case class ThreadSafeQueue[A](init: Seq[A]) {
   private[this] val queue = new LinkedBlockingQueue[A](init.asJava)
   def poll(): Option[A] = Option(queue.poll())
